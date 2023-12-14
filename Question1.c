e9697895e2546df9450bde788bb14efd544f30af
oussndou77 <148482151+oussndou77@users.noreply.github.com>
2023-12-14 10:33:03 +0100

    Update Question1

--- Question1.c
+++ Question1.c
@@ -3,18 +3,24 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-
+    // Declare character arrays to store host and file information
     char host[20], file[20];
+    
+    // Copy command line arguments to the host and file arrays
     strcpy(host, argv[1]);
     strcpy(file, argv[2]);
 
+    // Display host information on the standard output
     write(STDOUT_FILENO, "Host : ", 7);
     write(STDOUT_FILENO, host, strlen(host));
     write(STDOUT_FILENO, "\n", 1);
 
+    // Display file information on the standard output
     write(STDOUT_FILENO, "File: ", 7);
     write(STDOUT_FILENO, file, strlen(file));
     write(STDOUT_FILENO, "\n", 1);
 
+    // Program execution success
     return 0;
 }
+
