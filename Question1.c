357007caa4ac5ed24aea7387a96ade910516de2d
oussndou77 <148482151+oussndou77@users.noreply.github.com>
2023-12-14 10:21:20 +0100

    Update Question1

--- Question1.c
+++ Question1.c
@@ -1,1 +1,20 @@
-// QUESTION1
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+
+    char host[20], file[20];
+    strcpy(host, argv[1]);
+    strcpy(file, argv[2]);
+
+    write(STDOUT_FILENO, "Host : ", 7);
+    write(STDOUT_FILENO, host, strlen(host));
+    write(STDOUT_FILENO, "\n", 1);
+
+    write(STDOUT_FILENO, "File: ", 7);
+    write(STDOUT_FILENO, file, strlen(file));
+    write(STDOUT_FILENO, "\n", 1);
+
+    return 0;
+}
