c051055df59d0563cb4815e932d64b405b7df35f
oussndou77 <148482151+oussndou77@users.noreply.github.com>
2023-12-21 16:02:55 +0100

    UPDATE QUESTION 1

--- Question1.c
+++ Question1.c
@@ -3,12 +3,19 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-    // Declare character arrays to store host and file information
-    char host[20], file[20];
-    
-    // Copy command line arguments to the host and file arrays
+    // Check if the correct number of command-line arguments is provided
+    if (argc < 4) {
+        printf("Error: Insufficient arguments. Usage: %s <server> <file> <port>\n", argv[0]);
+        return 1; // Return an error code indicating failure
+    }
+
+    // Declare character arrays to store host, file, and port information
+    char host[20], file[20], port[20];
+
+    // Copy command line arguments to the host, file, and port arrays
     strcpy(host, argv[1]);
     strcpy(file, argv[2]);
+    strcpy(port, argv[3]);
 
     // Display host information on the standard output
     write(STDOUT_FILENO, "Host : ", 7);
@@ -20,7 +27,11 @@ int main(int argc, char *argv[]) {
     write(STDOUT_FILENO, file, strlen(file));
     write(STDOUT_FILENO, "\n", 1);
 
+    // Display port information on the standard output
+    write(STDOUT_FILENO, "Port: ", 7);
+    write(STDOUT_FILENO, port, strlen(port));
+    write(STDOUT_FILENO, "\n", 1);
+
     // Program execution success
     return 0;
 }
-
